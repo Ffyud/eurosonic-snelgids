343d7679301e846fb9511652cd4ab264d9fc2897
David <5263778+Ffyud@users.noreply.github.com>
2024-11-19 23:18:32 +0100

    clean up

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,7 @@
-import { Component, inject, Input, computed, Signal } from '@angular/core';
+import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { SelectDayComponent } from "./select-day/select-day.component";
 // import { SelectLocationComponent } from "./select-location/select-location.component";
-import { SnelgidsService } from './snelgids.service';
-import { Gig } from './gig.model';
-import { Location } from './location.enum';
 import { EventListComponent } from './event-list/event-list.component';
 import { TapBarNavComponent } from "./tap-bar-nav/tap-bar-nav.component";
 
