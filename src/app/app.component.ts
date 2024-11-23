e47501a18a79428fccb701bd1798769fd58d81e7
David <5263778+Ffyud@users.noreply.github.com>
2024-11-23 23:40:18 +0100

    use qr-code component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, signal, WritableSignal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { SelectDayComponent } from "./select-day/select-day.component";
-// import { SelectLocationComponent } from "./select-location/select-location.component";
-import { EventListComponent } from './event-list/event-list.component';
+// TODO import { SelectLocationComponent } from "./select-location/select-location.component";
 import { TapBarNavComponent } from "./tap-bar-nav/tap-bar-nav.component";
+import { DialogQrCodeComponent } from "./dialog-qr-code/dialog-qr-code.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, SelectDayComponent, EventListComponent, TapBarNavComponent],
+  imports: [RouterOutlet, TapBarNavComponent, DialogQrCodeComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'eurosonic-snelgids';
-  
+
+  protected dialogQrCodeIsOpen: WritableSignal<boolean> = signal(false);
+
+  protected openDialogQrCode() {
+    console.log('klik', this.dialogQrCodeIsOpen());
+    this.dialogQrCodeIsOpen() ? this.dialogQrCodeIsOpen.set(false) : this.dialogQrCodeIsOpen.set(true);
+  }
+
+  protected closeDialogQrCode() {
+    this.dialogQrCodeIsOpen.set(false);
+  }
+
 }
