bb0aa4d62b16c31c0988cf04449b9164699716e8
David <5263778+Ffyud@users.noreply.github.com>
2026-01-06 21:37:15 +0100

    nieuwe snelgids data gebruiken

--- src/app/snelgids.service.ts
+++ src/app/snelgids.service.ts
@@ -1,5 +1,5 @@
 import { Injectable, signal, WritableSignal } from '@angular/core';
-import data from '../../sheet_data_2025.json';
+import data from '../../sheet_data_2026.json';
 import { Gig } from './gig.model';
 import { Day } from './day.enum';
 import { Location } from './location.enum';
